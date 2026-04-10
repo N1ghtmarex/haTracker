a83c8814ec6a7681fff0353767ad1f9b958c1231
N1ghtmarex <andreyshe22@gmail.com>
2026-04-10 15:48:53 +0700

    feat: добавлен ApplicationDbContext

---
+++ Domain/ApplicationDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Domain;
+
+internal class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("ConnectionString"));
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
