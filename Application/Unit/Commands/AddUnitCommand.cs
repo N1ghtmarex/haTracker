6ba460a5bbc1034d4a6382efbaf382317a58d21f
N1ghtmarex <andreyshe22@gmail.com>
2026-04-11 11:26:38 +0700

    feat: добавлено создание и получение единиц измерения

---
+++ Application/Unit/Commands/AddUnitCommand.cs
@@ -0,0 +1,13 @@
+﻿using Application.Unit.Dtos;
+using Core.Utils;
+using MediatR;
+
+namespace Application.Unit.Commands;
+
+public class AddUnitCommand : IRequest<Result<Ulid>>
+{
+    /// <summary>
+    /// Модель запроса
+    /// </summary>
+    public required AddUnitModel Body { get; init; }
+}
---
+++ Application/Unit/Queries/GetUnitListQuery.cs
@@ -0,0 +1,11 @@
+﻿using Application.Unit.Dtos;
+using Core.BaseModels;
+using Core.EntityFramework.Features.SearchPagination.Models;
+using Core.Utils;
+using MediatR;
+
+namespace Application.Unit.Queries;
+
+public class GetUnitListQuery : SearchablePagedQuery, IRequest<Result<PagedResult<UnitViewModel>>>
+{
+}
